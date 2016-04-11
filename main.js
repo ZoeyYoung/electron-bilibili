ecec57e4b5426f2608c6107ed6ed4869207cb2c8
ZoeyYoung <ydingmiao@gmail.com>
2016-04-11 23:30:51 +0800

    使用loadURL

--- main.js
+++ main.js
@@ -1,19 +1,24 @@
 'use strict';
 
 const electron = require('electron')
+const app = electron.app; // Module to control application life.
+const BrowserWindow = electron.BrowserWindow;  // Module to create native browser window.
 
-const app = electron.app;
+let mainWindow = null;
 
-let mainWindow;
-
-app.on('ready', function() {
-  createWindow();
-});
-
+// Quit when all windows are closed.
 app.on('window-all-closed', function() {
-  if (process.platform !== 'darwin') {
+  // On OS X it is common for applications and their menu bar
+  // to stay active until the user quits explicitly with Cmd + Q
+  if (process.platform != 'darwin') {
     app.quit();
   }
+});
+
+// This method will be called when Electron has finished
+// initialization and is ready to create browser windows.
+app.on('ready', function() {
+  createWindow();
 });
 
 app.on('activate', function() {
@@ -23,18 +28,21 @@ app.on('activate', function() {
 });
 
 function createWindow() {
-  mainWindow = new electron.BrowserWindow({
-    "min-width"         : 800,
-    "min-height"        : 600,
-    fullscreen          : true,
-    resizable           : true,
-    "use-content-size"  : true,
+  // @see https://github.com/electron/electron/blob/master/docs%2Fapi%2Fbrowser-window.md
+  mainWindow = new BrowserWindow({
+    width: 1280,
+    minWidth: 1280,
+    minHeight: 600,
+    resizable: true,
+    useContentSize: true,
+    alwaysOnTop: true,
     title: '哔哩哔哩',
     icon: __dirname + '/bilibili.png'
   });
+  mainWindow.loadURL("http://www.bilibili.com/");
+  // mainWindow.loadURL('file://' + __dirname + '/index.html');
 
-  mainWindow.loadURL('file://' + __dirname + '/index.html');
-
+  // Emitted when the window is closed.
   mainWindow.on('closed', function() {
     mainWindow = null;
   });
--- onload.js
+++
@@ -1,5 +0,0 @@
-'use strict';
-
-onload = function() {
-  const webview = document.querySelector('webview');
-};
